36efd18cab916b7c90c49c5b21e1a5adfa4826ba
shinjh0849 <21200421@handong.edu>
2017-10-26 11:31:59 +0900

    Jiho

--- src/pages/nfc-menu/nfc-menu.ts
+++ src/pages/nfc-menu/nfc-menu.ts
@@ -1,27 +1,60 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, Alert } from 'ionic-angular';
 import { NFC, Ndef } from '@ionic-native/nfc';
-
+import { TagUtil, Tag } from './tag';
+import { StorageUtils } from './storageutils';
 @IonicPage()
 @Component({
   selector: 'page-nfc-menu',
   templateUrl: 'nfc-menu.html',
 })
 export class NfcMenuPage {
+  dataReceived: boolean;
+  showAnimation: boolean = false;
+  tag: Tag;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private nfc: NFC, private ndef: Ndef) {
+  constructor(public nav: NavController, public navParams: NavParams, private nfc: NFC, private ndef: Ndef, public alert: Alert) {
+    this.dataReceived = false;
+    this.tag = new Tag();
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad NfcMenuPage');
   }
 
-
-  add(){
-    this.nfc.addNdefListener(this.callback)
+  addNfcListeners(): void {
+    this.nfc.addTagDiscoveredListener((tagEvent: Event) => this.tagListenerSuccess(tagEvent));
+    this.nfc.addNdefListener((tagEvent: Event) => this.tagListenerSuccess(tagEvent));
   }
-  
-  callback(){
-    console.log('addNdefListener Successed!')
+
+  tagListenerSuccess(tagEvent: Event) {
+    console.log(tagEvent);
+    this.vibrate(2000);
+  }
+  vibrate(time: number): void {
+    if (navigator.vibrate) {
+      navigator.vibrate(time);
+    }
+  }
+
+  saveTag(): void {
+    if (this.tag.id) {
+      this.tag.key = btoa(this.tag.id[1]);
+
+      if (!StorageUtils.hasTags()) {
+        StorageUtils.setTags([]);
+      }
+
+      let tags: Array<any> = StorageUtils.getTags();
+      tags = tags.filter((item) => item.key !== this.tag.key);
+
+      this.tag.date = new Date().toISOString();
+      tags.push(this.tag);
+
+      StorageUtils.setTags(tags);
+
+
+    }
+
   }
 }
---
+++ src/pages/nfc-menu/tags.ts
@@ -0,0 +1,17 @@
+
+import { Inject, NgZone } from '@angular/core';
+
+import { StorageUtils } from './storageutils';
+
+export class TagsPage {
+    tags: Array<any>;
+    constructor() {
+        this.getTags();
+    }
+    getTags(): void {
+        this.tags = StorageUtils.getTags();
+        if (this.tags.length > 0) {
+            this.tags.forEach((tag) => tag.date = new Date(tag.date));
+        }
+    }
+}
